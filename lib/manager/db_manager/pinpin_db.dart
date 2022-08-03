f9ac8a4a2ee607adff1a9e05762c4d4e8e505a6d
Hayden-J <1761373255@qq.com>
2022-08-04 00:36:50 +0800

    init db manager and settings manager

--- lib/manager/db_manager/pinpin_db.dart
+++ lib/manager/db_manager/pinpin_db.dart
@@ -1,8 +1,36 @@
 /// pinpin - pinpin_db 
 /// Created by xhz on 2022/7/28
 import 'package:get/get.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 
 class PPDBManager extends GetxService {
+  late final Database _database;
+  static const _databaseName = 'pinpin.db';
+  static const _pinpinTableName = 'pinpin';
 
+  Future<void> createTables(Database db, int version) async {
+    // return db.execute(
+    //   '''
+    //       CREATE TABLE $_pinpinTableName
+    //       (
+    //         id INTEGER NOT NULL
+    //           CONSTRAINT ${_pinpinTableName}_pk
+    //             PRIMARY KEY AUTOINCREMENT,
+    //         pinpin_id INTEGER NOT NULL,
+    //       );
+    //       CREATE UNIQUE INDEX ${_pinpinTableName}_pinpin_id_uindex ON $_pinpinTableName (pinpin_id);
+    //       ''',
+    // );
+  }
+
+  Future<PPDBManager> init() async {
+    _database = await openDatabase(
+      join(await getDatabasesPath(), _databaseName),
+      onCreate: createTables,
+      version: 1,
+    );
+    return this;
+  }
 }
