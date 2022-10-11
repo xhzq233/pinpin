e371bdaf173f2cf231bf0cd609f30caf10854c9e
whl <1677378469@qq.com>
2022-10-11 21:24:49 +0800

    feat: add profile page

--- lib/page/profile/view.dart
+++ lib/page/profile/view.dart
@@ -33,6 +33,63 @@ class ProfilePage extends StatelessWidget {
         ownerEmail: "U202013777",
         updatedAt: 555,
         isFollowed: true);
+
+    /// content
+    const personDesc = Text(
+      "个人简介",
+      style: AppTheme.headline4,
+      textAlign: TextAlign.left,
+    );
+
+    const labels = Text(
+      "标签页",
+      style: AppTheme.headline4,
+      textAlign: TextAlign.left,
+    );
+
+    const text = Text(
+      "多好玩会晚点海王awful啊文本色废弃物画的图还挺好都挺好挺好好更好地发放我发哇哇哇大发发阿尔法啊发的，而非爱儿阿苏是的污。",
+      style: AppTheme.headline9,
+      textAlign: TextAlign.left,
+    );
+
+    const launchers = Text(
+      "我发布的",
+      style: AppTheme.headline4,
+      textAlign: TextAlign.left,
+    );
+
+    const checkAll = SizedBox(
+      width: 48,
+      height: 17,
+      child: Text(
+        "查看全部",
+        style: AppTheme.headline9,
+        textAlign: TextAlign.left,
+      ),
+    );
+
+    final rightArrow = Image.asset(
+      AppAssets.arrow_right,
+      height: 28,
+      fit: BoxFit.fitHeight,
+    );
+
+    final box =
+        Row(mainAxisSize: MainAxisSize.max, children: [checkAll, rightArrow]);
+
+    final lock = Image.asset(
+      AppAssets.lock,
+      height: 20,
+      fit: BoxFit.fitHeight,
+    );
+
+    const all_can_see = Text(
+      "所有人可见",
+      style: AppTheme.headline10,
+      textAlign: TextAlign.left,
+    );
+
     return Scaffold(
         body: NestedScrollView(
       headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
@@ -40,7 +97,96 @@ class ProfilePage extends StatelessWidget {
           const SliverPersistentHeader(
             pinned: true,
             delegate: PinPinPersonSliverHeaderDelegate(),
-          )
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [personDesc],
+                ),
+                Container(
+                    width: 354,
+                    height: 70,
+                    padding: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Colors.white,
+                    ),
+                    child: text),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [labels],
+                ),
+                Wrap(
+                  spacing: 8.0, // 主轴(水平)方向间距
+                  runSpacing: 0.5, // 纵轴（垂直）方向间距
+                  alignment: WrapAlignment.start, //沿主轴方向居中
+                  children: <Widget>[
+                    const Chip(
+                      labelStyle:
+                          TextStyle(color: Color(0xFF0076FC), fontSize: 12),
+                      label: Text("白羊座"),
+                    ),
+                    const Chip(
+                      labelStyle:
+                          TextStyle(color: Color(0xFF0076FC), fontSize: 12),
+                      label: Text('二次元'),
+                    ),
+                    const Chip(
+                      labelStyle:
+                          TextStyle(color: Color(0xFF0076FC), fontSize: 12),
+                      label: Text('intp'),
+                    ),
+                    const Chip(
+                      labelStyle:
+                          TextStyle(color: Color(0xFF0076FC), fontSize: 12),
+                      label: Text('巴拉拉'),
+                    ),
+                    const Chip(
+                      labelStyle:
+                          TextStyle(color: Color(0xFF0076FC), fontSize: 12),
+                      label: Text('巴拉巴拉'),
+                    ),
+                    const Chip(
+                      labelStyle:
+                          TextStyle(color: Color(0xFF0076FC), fontSize: 12),
+                      label: Text('巴拉拉巴拉拉'),
+                    ),
+                    const Chip(
+                      labelStyle:
+                          TextStyle(color: Color(0xFF0076FC), fontSize: 12),
+                      label: Text('巴拉巴拉'),
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Align(
+                          alignment: Alignment.bottomLeft,
+                          child: launchers,
+                        ),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: box,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [lock, all_can_see],
+                    )
+                  ],
+                ),
+              ],
+            ).paddingSymmetric(horizontal: 18),
+          ),
         ];
       },
       body: ListView.builder(
